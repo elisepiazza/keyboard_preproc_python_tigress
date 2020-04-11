6c99602dacebd43223ef914e0707189293958631
Elise A. Piazza <epiazza@della5.princeton.edu>
2020-04-11 17:07:52 -0400

    Adapted for piano results

--- plot_isc_results.py
+++ plot_isc_results.py
@@ -8,26 +8,19 @@ group = 'AM'
 conds = ['I', '8B', '2B', '1B']
 
 
-I = np.load(datadir + 'I_iscs_' + group + '.npy')
-ISC_I = I[~np.isnan(I)]
-
-8B = np.load(datadir + '8B_iscs_' + group + '.npy')
-ISC_8B = 8B[~np.isnan(8B)]
-
-2B = np.load(datadir + '2B_iscs_' + group + '.npy')
-ISC_2B = 2B[~np.isnan(2B)]
-
-1B = np.load(datadir + '1B_iscs_' + group + '.npy')
-ISC_1B = 1B[~np.isnan(1B)]
+ISC_I = np.load(datadir + 'I_iscs_' + group + '.npy')
+ISC_8B = np.load(datadir + '8B_iscs_' + group + '.npy')
+ISC_2B = np.load(datadir + '2B_iscs_' + group + '.npy')
+ISC_1B = np.load(datadir + '1B_iscs_' + group + '.npy')
 
 
 # plot data and save figure
-x = np.arange(2)
-avgData = [np.mean(music_iscs),np.mean(no_music_iscs)]
-semData = [stats.sem(music_iscs,axis=None,ddof=0),stats.sem(no_music_iscs,axis=None,ddof=0)]
+x = np.arange(4)
+avgData = [np.mean(ISC_I),np.mean(ISC_8B),np.mean(ISC_2B),np.mean(ISC_1B)]
+semData = [stats.sem(ISC_I,axis=None,ddof=0),stats.sem(ISC_8B,axis=None,ddof=0),stats.sem(ISC_2B,axis=None,ddof=0),stats.sem(ISC_1B,axis=None,ddof=0)]
 plt.bar(x,avgData,yerr=semData)
-plt.xticks(x,('music','no music'),fontsize=15)
-plt.title('ISC for Full Movie (rA1)',fontsize=20)
+plt.xticks(x,('I','8B','2B','1B'),fontsize=15)
+plt.title('ISC Across Scrambled Conditions (rA1)',fontsize=20)
 plt.ylabel('ISC (r)',fontsize=15)
 plt.tight_layout()
-plt.savefig('isc_results_rA1')
+plt.savefig(datadir + 'isc_results_rA1')
