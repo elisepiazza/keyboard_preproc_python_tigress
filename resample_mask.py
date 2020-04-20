031efb8223025d723d0540b58ac40ec602b69f4c
Elise A. Piazza <epiazza@della5.princeton.edu>
2020-04-20 14:57:43 -0400

    Added Jamal's changes to use custom mask from all subjects

--- resample_mask.py
+++ resample_mask.py
@@ -1,14 +1,15 @@
 import numpy as np
 import nibabel as nib
 from nilearn.image import resample_img
+import nilearn.masking
 
-# load output affine matrix
-output_affine = nib.load('/tigress/epiazza/keyboard/rois/MNI152_T1_3mm_brain.nii.gz').affine
+# load custom mask
+custom_mask = nib.load('/tigress/epiazza/keyboard/rois/custom_mask.nii.gz')
 
 # load data to be resampled
-input_data = nib.load('/tigress/epiazza/keyboard/rois/vmPFC_mask.nii.gz')
+input_data = nib.load('/tigress/epiazza/keyboard/rois/a1plus_2mm.nii.gz')
 
-img_in_mm_space = resample_img(input_data, target_affine=output_affine,
+img_in_mm_space = resample_img(input_data, target_affine=custom_mask.affine,
                                target_shape=(65, 77, 65))
 
 # binarize mask
@@ -19,9 +20,13 @@ mask_bin = np.reshape(mask_no_bin, (65, 77, 65))
 # save mask
 maxval = np.max(mask_bin)
 minval = np.min(mask_bin)
-img = nib.Nifti1Image(mask_bin, affine=output_affine)
+img = nib.Nifti1Image(mask_bin, affine=custom_mask.affine)
 img.header['cal_min'] = minval
 img.header['cal_max'] = maxval
-nib.save(img, '/tigress/epiazza/keyboard/rois/vmPFC_mask_3mm.nii.gz')
 
+# find intersection of custom mask and resampled mask
+mask_lst = [custom_mask, img]
+custom_resampled_msk = nilearn.masking.intersect_masks(mask_lst,threshold=1)
+
+nib.save(custom_resampled_msk, '/tigress/epiazza/keyboard/rois/a1plus_3mm_custom.nii.gz')
 
