68966f6a35c6b6920898bf19d76b70b474f680a3
Elise A. Piazza <epiazza@della5.princeton.edu>
2020-04-21 17:04:59 -0400

    Added fix for rep-specific matrix

--- piano_isc.py
+++ piano_isc.py
@@ -15,7 +15,7 @@ import pandas as pd
 #subjs = ['sub-103','sub-105','sub-108','sub-117','sub-120','sub-121', 'sub-122', 'sub-123']
 #groups = ['AM', 'M', 'M', 'M', 'AM', 'M', 'M', 'AM']
 
-ROI = 'a1plus'
+ROI = 'Precuneus'
 
 group = 'AM'
 subjs = ['sub-103', 'sub-120', 'sub-123']
@@ -26,7 +26,7 @@ subjs = ['sub-103', 'sub-120', 'sub-123']
 datadir = '/tigress/epiazza/keyboard/data/'
 save_dir = '/tigress/epiazza/keyboard/results/isc/'
 
-mask = nib.load('/tigress/epiazza/keyboard/rois/a1plus_3mm_custom.nii.gz')
+mask = nib.load('/tigress/epiazza/keyboard/rois/Precuneus_3mm_custom.nii')
 mask_size = mask.get_data()[mask.get_data()==1].shape[0]
 condition_data = pd.read_csv('/tigress/epiazza/keyboard/data/Conditions.csv')
 
@@ -42,7 +42,8 @@ nTR = 148
 # Structure data for brainiak isc function
 for c in range(len(conds)):
     # Initialize data structure for each condition separately, which will go into ISC
-    data = np.empty((nTR, mask_size, len(subjs)))    
+    data = np.empty((nTR, mask_size, len(subjs)))  
+    data_indvReps = np.empty((nTR, mask_size, nReps, len(subjs)))  
     for s in range(len(subjs)):
         # Select subject-specific column containing condition labels
         subj_col = condition_data.loc[:, subjs[s]]
@@ -50,8 +51,7 @@ for c in range(len(conds)):
         idx = np.where(subj_col == conds[c])[0] + 1
         # Initialize structure for storing average run data across subjects
         avgRuns = np.empty((nTR,mask_size))
-        Runs3D = np.empty((nTR,mask_size,nReps))
-        indvReps = np.empty((nTR, mask_size, len(subjs), nReps))    
+        Runs3D = np.empty((nTR,mask_size,nReps)) 
         for r in range(len(idx)):
             # Load run data 
             runData = nib.load(datadir + subjs[s] + '/clean_data/clean_data_run' + str(idx[r]) + '.nii.gz')
@@ -59,8 +59,14 @@ for c in range(len(conds)):
             masked_data = apply_mask(runData, mask)
             # store masked data for each run separately
             Runs3D[:,:,r] = masked_data
-        # Store average run data for each subject separately
+        # Store rep-specific data for each subject
+        data_indvReps[:,:,:,s] = Runs3D      
+        # Store averaged run data for each subject separately
         data[:,:,s] = np.mean(Runs3D,axis=2)  
+
+    #Save (voxel-averaged) rep-specific data
+    avgData_indvReps = np.mean(data_indvReps,axis=1,keepdims=False)
+    np.save(save_dir + ROI + '/' + conds[c] + '_avgData_indvReps_' + group, avgData_indvReps)   
 
     # Average over all voxels before feeding to ISC   
     avgData = np.mean(data,axis=1,keepdims=True) 
@@ -69,7 +75,7 @@ for c in range(len(conds)):
     # Save ISCs 
     np.save(save_dir + ROI + '/' + conds[c] + '_iscs_' + group, iscs) 
 
-    #Remove 3rd (singleton) dim and save individual time series
+    #Remove 3rd (singleton) dim and save rep-averaged data
     avgData_tosave = np.squeeze(avgData) 
     np.savetxt(save_dir + ROI + '/' + conds[c] + '_avgData_' + group + '.txt', avgData_tosave, delimiter =',')
     np.save(save_dir + ROI + '/' + conds[c] + '_avgData_' + group, avgData_tosave) 
